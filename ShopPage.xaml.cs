61e5ccbba7ac3d70aae4cff0687f1849a1263b9f
Paul Buda <paul.bg94@gmail.com>
2026-01-15 23:13:25 +0200

    laborator10: 15-20

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,4 +1,5 @@
 using BudaPaulLab7.Models;
+using Plugin.LocalNotification;
 
 namespace BudaPaulLab7;
 
@@ -26,6 +27,20 @@ public partial class ShopPage : ContentPage
 		var location = locations?.FirstOrDefault();
 		// var myLocation = await Geolocation.GetLocationAsync();
 		var myLocation = new Location(46.7731796289, 23.6213886738);
+		var distance = myLocation.CalculateDistance(location, DistanceUnits.Kilometers);
+		if (distance < 4)
+		{
+			var request = new NotificationRequest
+			{
+				Title = "Ai de facut cumparaturi in apropiere!",
+				Description = address,
+				Schedule = new NotificationRequestSchedule
+				{
+					NotifyTime = DateTime.Now.AddSeconds(1)
+				}
+			};
+			LocalNotificationCenter.Current.Show(request);
+		}
 		await Map.Default.OpenAsync(location, options);
 	}
 }
