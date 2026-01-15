6c6c8290095bd3601ce3e3b91a08b2eabe13787e
Paul Buda <paul.bg94@gmail.com>
2026-01-15 22:11:41 +0200

    Laborator 9: 1-19

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace BudaPaulLab7.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+    }
+}
