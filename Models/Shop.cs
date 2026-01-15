6c7bc6e006a94690cceb63d4572cf4cec3c4af46
Paul Buda <paul.bg94@gmail.com>
2026-01-15 22:38:01 +0200

    laborator10: 1-7

---
+++ Models/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace BudaPaulLab7.Models
+{
+    public class Shop
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string ShopName { get; set; }
+        public string Adress { get; set; }
+        public string ShopDetails { get { return ShopName + " " + Adress; } }
+        [OneToMany]
+        public List<ShopList> ShopLists { get; set; }
+    }
+}
--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace BudaPaulLab7.Models
 {
@@ -12,5 +13,7 @@ namespace BudaPaulLab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
     }
 }
